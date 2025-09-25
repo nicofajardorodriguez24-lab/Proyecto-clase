57cc569dc82c8154c9aacb51f5dd422c4cb31720
nicofajardorodriguez24-lab <nicofajardorodriguez24@gmail.com>
2025-09-25 09:59:39 -0500

    Update Pais.java

--- actividad3/model/Pais.java
+++ actividad3/model/Pais.java
@@ -1,54 +1,54 @@
 package co.edu.poli.actividad3.model;
 
+/**
+ * Clase que representa un país dentro del sistema de medicamentos.
+ * <p>
+ * Se utiliza como parte de la información de un proveedor.
+ * </p>
+ * 
+ * @author Nicolas
+ */
 public class Pais {
 
-	private String idPais;
+    /** Identificador único del país */
+    private String idPais;
+
+    /** Nombre del país */
     private String nombre;
-	
-    
-    
-    
+
+    /**
+     * Constructor que inicializa un país con su identificador y nombre.
+     *
+     * @param idPais identificador único del país
+     * @param nombre nombre del país
+     */
     public Pais(String idPais, String nombre) {
-		super();
-		this.idPais = idPais;
-		this.nombre = nombre;
-	}
+        super();
+        this.idPais = idPais;
+        this.nombre = nombre;
+    }
 
+    // ================== Getters & Setters ==================
 
+    public String getIdPais() {
+        return idPais;
+    }
 
+    public void setIdPais(String idPais) {
+        this.idPais = idPais;
+    }
 
-	public String getIdPais() {
-		return idPais;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-
-
-	public void setIdPais(String idPais) {
-		this.idPais = idPais;
-	}
-
-
-
-
-	public String getNombre() {
-		return nombre;
-	}
-
-
-
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-
-
-
-	@Override
-	public String toString() {
-		return "Pais [idPais=" + idPais + ", nombre=" + nombre + "]";
-	}
-
-    
+    //  toString 
+    @Override
+    public String toString() {
+        return "Pais [idPais=" + idPais + ", nombre=" + nombre + "]";
+    }
 }
