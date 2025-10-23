ff16c14c87457fd6e4275af2b477e21defbd37a8
nicofajardorodriguez24-lab <nicofajardorodriguez24@gmail.com>
2025-10-23 10:11:27 -0500

    Update Receta.java

--- actividad3/model/Receta.java
+++ actividad3/model/Receta.java
@@ -1,11 +1,14 @@
-package co.edu.poli.actividad3.model;
+package Co.edu.poli.actividad1.model;
+
+import java.io.Serializable;
 
 /**
  * Clase que representa una receta medica
  * Contiene informacion sobre la dosis, el paciente y el metodo de aplicacion
- * @author Nicolas Fajardo
  */
-public class Receta {
+public class Receta implements Serializable {
+
+    private static final long serialVersionUID = 1L; // control de version
 
     private String serial;
     private int dosis;
@@ -17,14 +20,6 @@ public class Receta {
 
     /**
      * Constructor con parametros de la clase Receta
-     * @param serial identificador de la receta
-     * @param dosis dosis indicada en la receta
-     * @param peso peso del paciente
-     * @param nombre nombre del paciente
-     * @param origen origen de la receta
-     * @param metodoAplicacion metodo de aplicacion del medicamento
-     * @param fechaOrigen anio de origen de la receta
-     * @author Nicolas Fajardo
      */
     public Receta(String serial, int dosis, int peso, String nombre, String origen, String metodoAplicacion, int fechaOrigen) {
         this.serial = serial;
@@ -36,80 +31,23 @@ public class Receta {
         this.fechaOrigen = fechaOrigen;
     }
 
-    /**
-     * @return serial de la receta
-     */
+
     public String getSerial() { return serial; }
-
-    /**
-     * @param serial serial a asignar
-     */
     public void setSerial(String serial) { this.serial = serial; }
-
-    /**
-     * @return dosis indicada
-     */
     public int getDosis() { return dosis; }
-
-    /**
-     * @param dosis dosis a asignar
-     */
     public void setDosis(int dosis) { this.dosis = dosis; }
-
-    /**
-     * @return peso del paciente
-     */
     public int getPeso() { return peso; }
-
-    /**
-     * @param peso peso a asignar
-     */
     public void setPeso(int peso) { this.peso = peso; }
-
-    /**
-     * @return nombre del paciente
-     */
     public String getNombre() { return nombre; }
-
-    /**
-     * @param nombre nombre a asignar
-     */
     public void setNombre(String nombre) { this.nombre = nombre; }
-
-    /**
-     * @return origen de la receta
-     */
     public String getOrigen() { return origen; }
-
-    /**
-     * @param origen origen a asignar
-     */
     public void setOrigen(String origen) { this.origen = origen; }
-
-    /**
-     * @return metodo de aplicacion del medicamento
-     */
     public String getMetodoAplicacion() { return metodoAplicacion; }
-
-    /**
-     * @param metodoAplicacion metodo de aplicacion a asignar
-     */
     public void setMetodoAplicacion(String metodoAplicacion) { this.metodoAplicacion = metodoAplicacion; }
-
-    /**
-     * @return anio de origen de la receta
-     */
     public int getFechaOrigen() { return fechaOrigen; }
-
-    /**
-     * @param fechaOrigen anio de origen a asignar
-     */
     public void setFechaOrigen(int fechaOrigen) { this.fechaOrigen = fechaOrigen; }
 
-    /**
-     * Representacion en texto de la receta
-     * @return cadena con los datos de la receta
-     */
+  
     @Override
     public String toString() {
         return "Receta {" +
