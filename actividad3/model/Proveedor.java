eb9d9c3ca99c8ca522a7e29a05e5430a247047fc
nicofajardorodriguez24-lab <nicofajardorodriguez24@gmail.com>
2025-10-23 10:11:08 -0500

    Update Proveedor.java

--- actividad3/model/Proveedor.java
+++ actividad3/model/Proveedor.java
@@ -1,11 +1,15 @@
-package co.edu.poli.actividad3.model;
+package Co.edu.poli.actividad1.model;
+
+import java.io.Serializable;
 
 /**
  * Clase que representa un proveedor de medicamentos
  * Contiene informacion sobre origen, certificaciones y pais de procedencia
  * @author Nicolas Fajardo
  */
-public class Proveedor {
+public class Proveedor implements Serializable {
+
+    private static final long serialVersionUID = 1L; // control de version
 
     private String nit;
     private String origen;
@@ -20,7 +24,6 @@ public class Proveedor {
      * @param producto producto que provee
      * @param certificacion certificacion de calidad
      * @param pais pais de procedencia
-     * @author Nicolas Fajardo
      */
     public Proveedor(String nit, String origen, String producto, String certificacion, Pais pais) {
         this.nit = nit;
@@ -30,60 +33,19 @@ public class Proveedor {
         this.pais = pais;
     }
 
-    /**
-     * @return nit del proveedor
-     */
+    
     public String getNit() { return nit; }
-
-    /**
-     * @param nit nit a asignar
-     */
     public void setNit(String nit) { this.nit = nit; }
-
-    /**
-     * @return origen del proveedor
-     */
     public String getOrigen() { return origen; }
-
-    /**
-     * @param origen origen a asignar
-     */
     public void setOrigen(String origen) { this.origen = origen; }
-
-    /**
-     * @return producto que provee
-     */
     public String getProducto() { return producto; }
-
-    /**
-     * @param producto producto a asignar
-     */
     public void setProducto(String producto) { this.producto = producto; }
-
-    /**
-     * @return certificacion del proveedor
-     */
     public String getCertificacion() { return certificacion; }
-
-    /**
-     * @param certificacion certificacion a asignar
-     */
     public void setCertificacion(String certificacion) { this.certificacion = certificacion; }
-
-    /**
-     * @return pais de procedencia
-     */
     public Pais getPais() { return pais; }
-
-    /**
-     * @param pais pais a asignar
-     */
     public void setPais(Pais pais) { this.pais = pais; }
 
-    /**
-     * Representacion en texto del proveedor
-     * @return cadena con los datos del proveedor
-     */
+   
     @Override
     public String toString() {
         return "Proveedor {" +
